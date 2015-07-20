95b33fd38f024fb1e1bf855ef7a7e4091a4fde99
travisjn <tdizzletrav@gmail.com>
2015-07-20 15:55:39 -0700

    Remove superfluous code

--- euler7/nthPrime.js
+++ euler7/nthPrime.js
@@ -34,28 +34,3 @@ var nthPrime = function(n) {
   return primes[primes.length - 1];
 };
 
-var checkPrime = function(number) {
-  if (number % 1 || number < 2) {
-    return false;
-  }
-
-  if (number % 2 === 0) {
-    return number === 2;
-  }
-  if (number % 3 === 0) {
-    return number === 3;
-  }
-
-  var m = Math.sqrt(number);
-
-  for (var i = 5; i <= m; i += 6) {
-    if (number % i === 0) {
-      return false;
-    }
-    if (number % (i + 2) === 0) {
-      return false;
-    }
-  }
-
-  return true;
-};
